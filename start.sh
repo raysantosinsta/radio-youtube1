99e413a36d99f24f255c89f1059559db14d8efa1
highlanderiniesta <rayiniesta007@gmail.com>
2026-09-14 03:10:22 -0300

    evitar travar

--- start.sh
+++ start.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-echo "Iniciando Rádio 24/7 (Modo de Baixo Consumo de CPU)..."
+echo "Iniciando Rádio 24/7 com correção de timestamps..."
 
-ffmpeg -re -stream_loop -1 -i "audio.MP3" \
--loop 1 -framerate 15 -i "back.png" \
+ffmpeg -re -fflags +genpts -loop 1 -framerate 30 -i "back.png" -re -stream_loop -1 -i "audio.MP3" \
 -c:v libx264 -preset ultrafast -tune stillimage -s 1280x720 -pix_fmt yuv420p \
--b:v 1500k -maxrate 1500k -bufsize 3000k -g 30 \
+-b:v 2000k -maxrate 2000k -bufsize 4000k -g 60 -keyint_min 60 \
 -c:a aac -b:a 128k -ar 44100 \
 -f flv "rtmp://a.rtmp.youtube.com/live2/$YOUTUBE_STREAM_KEY"
