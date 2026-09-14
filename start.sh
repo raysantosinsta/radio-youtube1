1b1c5596e14a5b02b63c85bb0d9c8d592015ed5f
highlanderiniesta <rayiniesta007@gmail.com>
2026-09-14 02:51:52 -0300

    LENTO

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-echo "Iniciando Rádio 24/7 (Padrão YouTube HD)..."
+echo "Iniciando Rádio 24/7 (Forçando velocidade Realtime)..."
 
-ffmpeg -loop 1 -framerate 30 -i "back.png" -stream_loop -1 -i "audio.MP3" \
+ffmpeg -re -loop 1 -framerate 30 -i "back.png" -re -stream_loop -1 -i "audio.mp3" \
 -vf "scale=1280:720,format=yuv420p" \
 -c:v libx264 -preset ultrafast -b:v 2500k -maxrate 2500k -bufsize 5000k \
 -g 60 -c:a aac -b:a 128k -ar 44100 \
