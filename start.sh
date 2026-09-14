f182f67d5643009bd9a11aba246b9103aae24920
highlanderiniesta <rayiniesta007@gmail.com>
2026-09-14 03:28:08 -0300

    4s

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "Iniciando Rádio 24/7 (Padrão YouTube HD)..."
 
-ffmpeg -loop 1 -framerate 30 -i "back.png" -stream_loop -1 -i "audio.MP3" \
+ffmpeg -re -loop 1 -framerate 30 -i "back.png" -re -stream_loop -1 -i "audio.MP3" \
 -vf "scale=1280:720,format=yuv420p" \
 -c:v libx264 -preset ultrafast -b:v 2500k -maxrate 2500k -bufsize 5000k \
--g 60 -c:a aac -b:a 128k -ar 44100 \
+-g 120 -keyint_min 120 -c:a aac -b:a 128k -ar 44100 \
 -f flv "rtmp://a.rtmp.youtube.com/live2/$YOUTUBE_STREAM_KEY"
